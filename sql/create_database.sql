0e376c0f635ddea82e5aa2301b3b15749be16e32
Elly H <b.hatooka@gmail.com>
2021-02-21 19:24:39 +1100

    Initialised tables in Step 1

---
+++ sql/create_database.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS project_4;
+
+CREATE DATABASE project_4;
+\c project_4
--- sql/seed_users_table.sql
+++ sql/seed_users_table.sql
@@ -1,2 +1,13 @@
 INSERT INTO users (surname, firstname, email, psw)
-VALUES ("Anyone", "Someone", "anyemail@gmail.com", "pswrdsa12341dccv");
+VALUES ('Anyone', 'Someone', 'anyemail@gmail.com', 'pswrdsa12341dccv');
+
+INSERT INTO users (surname, firstname, email, psw)
+VALUES ('Elly', 'Someone1', 'any1email@gmail.com', 'blahblah');
+
+INSERT INTO schedules (ID_user, week_day, start_at, end_at)
+VALUES ('1', '2', '2021-06-22 12:10:25+11', '2021-06-22 19:10:00+11');
+
+INSERT INTO schedules (ID_user, week_day, start_at, end_at)
+VALUES ('2', '3', '2021-06-23 05:10:25+11', '2021-06-22 11:10:00+11');
+
+--SUCCESSFULLY IMPLEMENT TIMESTAMPTZ DATA
