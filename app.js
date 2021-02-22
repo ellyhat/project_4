b7b740259ca7788d80f008cd3df706cf20eb27b0
Elly H <b.hatooka@gmail.com>
2021-02-23 00:40:17 +1100

    Implement password and email check for Step 2

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 3000;
 const crypto = require("crypto");
 const morgan = require("morgan");
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 
+
 //Dotenv is a middle-ware that helps load variables from an .env file.
 
 const dotenv = require("dotenv");
 dotenv.config();
+const PORT = process.env.PORT;
+
 
 //The “require(‘dotenv’).config()” required the dotenv package and injects it into our project configuration.
 app.use(morgan("dev"));
 app.use(express.json());
 app.set("view engine", "ejs");
+
+//Set up Routers
+const loginRouter = require('./routes/login');
+app.use('/routes/login', loginRouter)
 
 //DESIGN
 app.use("/static", express.static(path.join(__dirname, "public")));
@@ -24,8 +30,10 @@ app.set("layout", "./layouts/full-width");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 //Add database
 const database = require("./database.js");
+console.log(database)
 
 //Test that connection to PORT is active
 app.listen(PORT, () => {
@@ -46,13 +54,18 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/login", (req, res) => {
+app.use("/login", loginRouter)
+
+
+
+
+/*app.get("/login", (req, res) => {
   res.render("pages/login", {
     title: "Form page",
   });
-});
+});*/
 
-app.post("/login", (req, res) => {
+/*app.post("/login", (req, res) => {
   const password = req.body.psw;
   const passwordEncr = crypto
     .createHash("sha256")
@@ -61,10 +74,10 @@ app.post("/login", (req, res) => {
   const currentUser = {
     email: req.body.email,
     psw: passwordEncr,
-  };
+  };*/
 
   //compare if there are any user like that
   //database.users=== currentUser ?
-  console.log(currentUser);
-  res.redirect("/");
-});
+  //console.log(currentUser);
+  //res.redirect("/");
+/*});*/
--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const pgp = require("pg-promise")();
+
 //const connection = "postgres://postgres:butzy@localhost:5432/project_4"
 //change to set the env variables - .env pack
 const connection = `${process.env.DB_CONNECTION}`;
