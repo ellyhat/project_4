22b28578d435c0a9c0d21860a958f346a1028e0b
Jane Kotovich <jenyakotovich@gmail.com>
2021-03-02 14:30:58 +1000

    hopefully it works

--- routes/users.js
+++ routes/users.js
@@ -4,24 +4,19 @@ const crypto = require("crypto");
 const app = express();
 const session = require("express-session");
 const database = require("../database.js");
-
-const redirectLogin = (req, res, next) => {
-  if (!req.session.userId) {
-    res.redirect("/login");
-  } else {
-    next();
-  }
-};
-
-router.get("/:userNum", redirectLogin, (req, res) => {
-  const userId = req.params.userNum;
-  const queryDisplayCombined = `SELECT surname, firstname, unique_key, week_day, TO_CHAR(start_at, 'HH:MI am') AS start_at, TO_CHAR(end_at, 'HH:MI am') as end_at, TO_CHAR(end_at, 'DD-Month-YYYY') as date FROM combined WHERE combined.user_ID = '${userId}' ORDER BY date;`;
+router.get("/:userNum", (req, res) => {
+  let userId = req.params.userNum;
+  console.log(userId);
+  query =
+    "SELECT surname, firstname,unique_key, week_day, TO_CHAR(start_at, 'HH:MI am') AS start_at, TO_CHAR(end_at, 'HH:MI am') as end_at, TO_CHAR(end_at, 'DD-Month-YYYY') as date FROM combined WHERE combined.user_ID = '" +
+    userId +
+    "' ORDER BY date;";
   database
-    .any(queryDisplayCombined)
+    .any(query)
     .then((scheduleTable) => {
       res.render("pages/users", {
         title: "Form page",
-        scheduleTable,
+        scheduleTable: scheduleTable,
       });
     })
     .catch((err) => {
@@ -30,12 +25,10 @@ router.get("/:userNum", redirectLogin, (req, res) => {
         layout: "./pages/layout",
         err: err.message,
       });
-    }); // THIS IS FINE
-});
-router.post("/", async (req, res) => {
-  try {
-    const { userId } = req.session;
-    // console.log(`hello + ${req.session.userId}`);
+    });
+  router.post("/", (req, res) => {
+    const userId = req.session.userId;
+    console.log(`hello + ${req.session.userId}`);
     const schedule = {
       start_at: req.body.starttime,
       end_at: req.body.endtime,
@@ -44,43 +37,46 @@ router.post("/", async (req, res) => {
     };
     const insertStart = `${schedule.date} ` + `${schedule.start_at}`;
     const insertEnd = `${schedule.date} ` + `${schedule.end_at}`;
-    /*	const queryInsertIntoSchedule =
-    'INSERT INTO schedules (user_id, week_day, start_at, end_at) VALUES($1,$2,$3,$4);'
-		const Insertschedule = await database.any(queryInsertIntoSchedule, [
-			userId,
-			schedule.week_day,
-			insertStart,
-			insertEnd,
-		])*/
-    const queryUserDisplay = `SELECT * FROM users WHERE user_id = '${userId}' ORDER BY firstname;`;
-    const user = await database.any(queryUserDisplay);
-    const userDetails = {
-      surname: user[0].surname,
-      firstname: user[0].firstname,
-      email: user[0].email,
-      psw: user[0].psw,
-    };
-    const query =
-      "INSERT INTO combined (user_id, surname, firstname, email, psw, week_day, start_at, end_at) VALUES($1,$2,$3,$4,$5,$6,$7,$8);";
-    const insetAll = await database.any(query, [
+    console.log(insertStart);
+    const queryInsertIntoSchedule =
+      "INSERT INTO schedules (user_id, week_day, start_at, end_at) VALUES($1,$2,$3,$4);";
+    database.any(queryInsertIntoSchedule, [
       userId,
-      userDetails.surname,
-      userDetails.firstname,
-      userDetails.email,
-      userDetails.psw,
       schedule.week_day,
       insertStart,
       insertEnd,
     ]);
-    const queryDisplayCombined = `SELECT surname, firstname, week_day, TO_CHAR(start_at, 'HH:MI am') AS start_at, TO_CHAR(end_at, 'HH:MI am') as end_at, TO_CHAR(end_at, 'DD-Month-YYYY') as date FROM combined WHERE combined.user_ID = '${userId}' ORDER BY date;`;
-    database.any(queryDisplayCombined).then((scheduleTable) => {
-      res.render("pages/users", {
-        title: "Form page",
-        scheduleTable,
-      });
+    // .then((response) => {SECOND DATABASE QUERY TO PUT}).then((SECOND THING)=>{})
+    // .catch((err) => {
+    //   res.render("./pages/error", {
+    //     title: "Error",
+    //     layout: "./pages/layout",
+    //     err: err.message,
+    //   });
+    // });
+    query = "SELECT * FROM users WHERE user_id = '" + userId + "' ;";
+    database.any(query).then((result) => {
+      const userDetails = {
+        surname: result[0].surname,
+        firstname: result[0].firstname,
+        email: result[0].email,
+        psw: result[0].psw,
+      };
+      console.log(userDetails.email);
+      console.log(result);
+      query =
+        "INSERT INTO combined (user_id, surname, firstname, email, psw, week_day, start_at, end_at) VALUES($1,$2,$3,$4,$5,$6,$7,$8);";
+      database.any(query, [
+        userId,
+        userDetails.surname,
+        userDetails.firstname,
+        userDetails.email,
+        userDetails.psw,
+        schedule.week_day,
+        insertStart,
+        insertEnd,
+      ]);
     });
-  } catch (error) {
-    console.log(error);
-  }
+  });
 });
 module.exports = router;
